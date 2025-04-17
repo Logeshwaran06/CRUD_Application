baf3de38a3778824cadc5422f22b889e714f6f29
Logeshwaran06 <logeshmohan4080@gmail.com>
2025-04-17 23:29:52 +0530

    badkend update for deploy on Render

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const userData = require('./sample.json'); // Load the JSON file
+const userData = require('./sample.json');
 const cors = require('cors');
 const port = 8000;
 const fs = require('fs');
@@ -8,28 +8,22 @@ const fs = require('fs');
 app.use(cors());
 app.use(express.json());
 
-// GET all users
 app.get('/users', (req, res) => {
   return res.json(userData);
 });
 
-// POST (Add a new user)
 app.post('/users', (req, res) => {
   const newUser = req.body;
 
-  // Validate input
   if (!newUser.name || !newUser.age || !newUser.city) {
     return res.status(400).json({ error: "All fields are required!" });
   }
 
-  // Generate a unique ID
   const maxId = userData.reduce((max, user) => (user.id > max ? user.id : max), 0);
   newUser.id = maxId + 1;
 
-  // Add the new user
   userData.push(newUser);
 
-  // Save to the JSON file
   fs.writeFile('./sample.json', JSON.stringify(userData), (err) => {
     if (err) {
       return res.status(500).json({ error: "Failed to add user!" });
@@ -38,22 +32,18 @@ app.post('/users', (req, res) => {
   });
 });
 
-// PUT (Update an existing user)
 app.put('/users/:id', (req, res) => {
   const { id } = req.params;
   const updatedUser = req.body;
 
-  // Find the user by ID
   const index = userData.findIndex(user => user.id === parseInt(id));
 
   if (index === -1) {
     return res.status(404).json({ error: "User not found!" });
   }
 
-  // Update the user
   userData[index] = { ...userData[index], ...updatedUser };
 
-  // Save to the JSON file
   fs.writeFile('./sample.json', JSON.stringify(userData), (err) => {
     if (err) {
       return res.status(500).json({ error: "Failed to update user!" });
@@ -62,21 +52,17 @@ app.put('/users/:id', (req, res) => {
   });
 });
 
-// DELETE (Remove a user)
 app.delete('/users/:id', (req, res) => {
   const { id } = req.params;
 
-  // Find the user by ID
   const index = userData.findIndex(user => user.id === parseInt(id));
 
   if (index === -1) {
     return res.status(404).json({ error: "User not found!" });
   }
 
-  // Remove the user
   userData.splice(index, 1);
 
-  // Save to the JSON file
   fs.writeFile('./sample.json', JSON.stringify(userData), (err) => {
     if (err) {
       return res.status(500).json({ error: "Failed to delete user!" });
@@ -85,7 +71,6 @@ app.delete('/users/:id', (req, res) => {
   });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
